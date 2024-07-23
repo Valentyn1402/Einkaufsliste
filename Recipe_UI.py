a2e37930739f7d1a00c55a0616ad23cb57406ab3
Valter Subotovic <ValterValentyn.Subotovic@infineon.com>
2024-07-23 11:51:42 +0200

    updated UI

--- Recipe_UI.py
+++ Recipe_UI.py
@@ -1,31 +1,47 @@
-import tkinter as tk
 from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
 #create the main window 
-window = Tk()
-window.title("Hello World")
-
+window = tk.Tk()
+window.title("Recipe Book")
 
 def handle_button_press():
     window.destroy()
 
 #define the labels in the window
-x1 = Label(window, text = "Enter the ingredient").grid(row=0)
-x1 = Label(window, text = "Enter the ammount").grid(row=1)
-  
+tk.Label(window, text = "Enter the recipe name: ").grid(row=0, column=0, padx=5, pady=5)
+tk.Label(window, text = "Enter the recipe category: ").grid(row=1, column=0, padx=5, pady=5)
+tk.Label(window, text = "Enter the ingredient: ").grid(row=2, column=0, padx=5, pady=5)
+tk.Label(window, text = "Enter the ingredient subcategory (optional): ").grid(row=3, column=0, padx=5, pady=5)
+tk.Label(window, text = "Enter the amount: ").grid(row=4, column=0, padx=5, pady=5)
+tk.Label(window, text = "in g").grid(row = 4, column = 2)
+tk.Label(window, text = "in ml").grid(row = 5, column = 2)
+tk.Label(window, text = "in units").grid(row = 6, column= 2)
 
 #define the entries in the window
-a = Entry(window)
-b = Entry(window)
-b1 = Entry(window)
-b2 = Entry(window)
-b3 = Entry(window)
+b0 = tk.Entry(window)
+b1 = tk.Entry(window)
+b2 = tk.Entry(window)
+b3 = tk.Entry(window)
+b4 = tk.Entry(window)
 
-a.grid(row = 0, column = 1)
-b.grid(row = 1, column = 1)
+b0.grid(row = 0, column = 1, padx=5, pady=5)
+b1.grid(row = 3, column = 1, padx=5, pady=5)
+b2.grid(row = 4, column = 1, padx=5, pady=5)
+b3.grid(row = 5, column = 1, padx=5, pady=5)
+b4.grid(row = 6, column = 1, padx=5, pady=5)
 
-button = tk.Button(text="My simple app.", command=handle_button_press)
-# button.pack()
+#define a button 
+button = tk.Button(text="Next ingredient", command=handle_button_press)
+button.grid(row = 7, column = 1, pady=10)
+
+#define a combobox
+c0 = ttk.Combobox(window, values=["Op1", "Op2", "Op3"])
+c1 = ttk.Combobox(window, values=["Breakfast", "Dinner"])
+
+c0.grid(row = 2, column = 1)
+c1.grid(row = 1, column = 1)
 
 # Start the event loop.
 window.mainloop()
