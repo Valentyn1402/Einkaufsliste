ecbc6a245db14d0b71c286679647575b944cc8eb
Valter Subotovic <ValterValentyn.Subotovic@infineon.com>
2024-09-05 15:20:50 +0200

    changed names + working UI

--- Recipe_UI.py
+++ Recipe_UI.py
@@ -1,19 +1,21 @@
-from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import os
-
 class UI():
 
     entries: list[tk.Entry]
     ingredient_list: list[str]
+    FILE_PATH: str
 
     def __init__(self):
 
         #create the main window 
         self.window = tk.Tk()
         self.window.title("Recipe Book")
+
+        #file path
+        FILE_PATH = "C:/Users/subotovic/Desktop/Code/Einkaufsliste/food.txt"
         
         #define instance variables
         self.flag: bool = False
@@ -21,7 +23,7 @@ class UI():
         self.ingredient_list = []
 
         #parse the file
-        self.parse_ingredients("C:/Users/subotovic/Desktop/Code/Einkaufsliste/food.txt")
+        self.parse_ingredients(FILE_PATH)
 
         #define string variables for the entries 
         self.vars = [tk.StringVar() for var in range(10)]
@@ -32,16 +34,33 @@ class UI():
         self.entries = [tk.Entry(self.window, textvariable=self.vars[i]) for i in range(5)]
 
         #position self.entries 
+        self.define_entries()
+
+        state = self.entries[0]
+
+        #bind buttons
+        self.bind_button()
+
+        #define the labels in the window
+        self.define_labels()
+
+        #define a button 
+        self.define_buttons()
+
+        #define a combobox
+        self.define_combobox()
+    
+        # Start the event loop.
+        self.window.mainloop()
+
+    def define_entries(self):
         self.entries[0].grid(row = 0, column = 1, padx=5, pady=5)
         self.entries[1].grid(row = 3, column = 1, padx=5, pady=5)
         self.entries[2].grid(row = 4, column = 1, padx=5, pady=5)
         self.entries[3].grid(row = 5, column = 1, padx=5, pady=5)
         self.entries[4].grid(row = 6, column = 1, padx=5, pady=5)
 
-        state = self.entries[0]
-        #bind Enter key to disable_entry function
-        self.window.bind('<Return>', self.handle_button_press)
- 
+    def define_labels(self):
         #define the labels in the window
         tk.Label(self.window, text = "Enter the recipe name: ").grid(row=0, column=0, padx=5, pady=5)
         tk.Label(self.window, text = "Enter the recipe category: ").grid(row=1, column=0, padx=5, pady=5)
@@ -52,10 +71,10 @@ class UI():
         tk.Label(self.window, text = "in ml").grid(row = 5, column = 2)
         tk.Label(self.window, text = "in units").grid(row = 6, column= 2)
 
-        #define a button 
-        button = tk.Button(text="Next ingredient", command=self.handle_button_press)
-        button.grid(row = 7, column = 1, pady=10)
+    def bind_button(self):
+        self.window.bind('<Return>', self.handle_button_press)
 
+    def define_combobox(self):
         #define a combobox
         self.c0 = ttk.Combobox(self.window, values=self.ingredient_list,  textvariable=self.combvar)
         self.c1 = ttk.Combobox(self.window, values=["breakfast", "dinner"], textvariable=self.combvar1)
@@ -63,8 +82,10 @@ class UI():
         self.c0.grid(row = 2, column = 1)
         self.c1.grid(row = 1, column = 1)
 
-        # Start the event loop.
-        self.window.mainloop()
+    def define_buttons(self):
+        #define a button 
+        button = tk.Button(text="Next ingredient", command=self.handle_button_press)
+        button.grid(row = 7, column = 1, pady=10)
 
     def parse_ingredients(self, text: str) -> None:
         #open the file
@@ -76,19 +97,6 @@ class UI():
         pass
 
     def disable_entry(self, event):
-        # if current_entry == "disabled":
-        #     self.entries[0] = current_entry
-        #     self.i += 1
-        #     current_entry = self.entries[self.i]
-        # else:
-        #     current_entry = self.entries[0]
-        # current_entry.config(state="disabled")
-
-        # make sure when the enter button is pressed that either
-        # recipe name is set as well as its category 
-        # also make sure that the amount field is populated so that the 
-        # amount of the ingredient is clear  
-
         #once button is pressed save the variable data to the list
         dir = "C:/Users/subotovic/Desktop/Code/Einkaufsliste/ingredient.txt"
         if not os.path.exists(dir):
@@ -118,7 +126,7 @@ class UI():
 
     #after the button is pressed save the variable data in 
     #specific format and reset the inactive windows
-    def handle_button_press(self):
+    def handle_button_press(self, event = None):
         #once button is pressed save the variable data to the list
         dir = "C:/Users/subotovic/Desktop/Code/Einkaufsliste/ingredient.txt"
         if not os.path.exists(dir):
@@ -153,57 +161,6 @@ class UI():
         [entry.delete(0, tk.END) for entry in self.entries[1:]]
 
         #disable the buttons and self.entries and clear them 
-                    
 
 if __name__ == "__main__":
     ui_instance = UI()
-
-# #create the main window 
-# window = tk.Tk()
-# window.title("Recipe Book")
-
-# flag: bool = False
-
-# i = 0
-
-# #define string variables for the entries 
-# vars = [tk.StringVar() for var in range(10)]
-# combvar = tk.StringVar()
-# combvar1 = tk.StringVar()
-
-# #define the entries in the window
-# entries = [tk.Entry(window, textvariable=vars[i]) for i in range(5)]
-
-# entries[0].grid(row = 0, column = 1, padx=5, pady=5)
-# entries[1].grid(row = 3, column = 1, padx=5, pady=5)
-# entries[2].grid(row = 4, column = 1, padx=5, pady=5)
-# entries[3].grid(row = 5, column = 1, padx=5, pady=5)
-# entries[4].grid(row = 6, column = 1, padx=5, pady=5)
-
-
-# #bind Enter key to disable_entry function
-# window.bind('<Return>', disable_entry) 
-
-# #define the labels in the window
-# tk.Label(window, text = "Enter the recipe name: ").grid(row=0, column=0, padx=5, pady=5)
-# tk.Label(window, text = "Enter the recipe category: ").grid(row=1, column=0, padx=5, pady=5)
-# tk.Label(window, text = "Enter the ingredient: ").grid(row=2, column=0, padx=5, pady=5)
-# tk.Label(window, text = "Enter the ingredient subcategory (optional): ").grid(row=3, column=0, padx=5, pady=5)
-# tk.Label(window, text = "Enter the amount: ").grid(row=4, column=0, padx=5, pady=5)
-# tk.Label(window, text = "in g").grid(row = 4, column = 2)
-# tk.Label(window, text = "in ml").grid(row = 5, column = 2)
-# tk.Label(window, text = "in units").grid(row = 6, column= 2)
-
-# #define a button 
-# button = tk.Button(text="Next ingredient", command=handle_button_press)
-# button.grid(row = 7, column = 1, pady=10)
-
-# #define a combobox
-# self.c0 = ttk.Combobox(window, values=["Op1", "Op2", "Op3"],  textvariable=combvar)
-# self.c1 = ttk.Combobox(window, values=["Breakfast", "Dinner"], textvariable=combvar1)
-
-# self.c0.grid(row = 2, column = 1)
-# self.c1.grid(row = 1, column = 1)
-
-# # Start the event loop.
-# window.mainloop()
