fb4e1d723c6d0b631bbaefd614f15fb8df4d8f70
Valter Subotovic <ValterValentyn.Subotovic@infineon.com>
2024-09-06 10:58:44 +0200

    functioning recipe entry dictionary, which contains all of the information about the ingredients

---
+++ parse_ingredients.py
@@ -0,0 +1,10 @@
+# class to parse the dictionary entries and either add them to the 
+# recipe list or not 
+# requires a list of requirements 
+
+
+# List of requirements:
+# Recipe has to have atleast 2 ingredients 
+# No two recipes can have the same name 
+# No two recipes can have identical list of ingredients 
+# 
