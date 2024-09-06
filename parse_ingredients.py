058d1e0d78dd39959dcfae9ca9697e60faf31691
Valter Subotovic <ValterValentyn.Subotovic@infineon.com>
2024-09-06 13:38:21 +0200

    working yaml parser

--- parse_ingredients.py
+++ parse_ingredients.py
@@ -30,29 +30,28 @@ class Parser():
         else: 
             return False
         
-    def check_ingredients(self, recipe: dict[str : str]) -> bool:
-        for entry in self.ingredient_file:
-            list_of_ingredients = entry["ingredients"]
-            recipe_ingrediets = recipe["ingredients"]
-            for ingredient in list_of_ingredients: 
-                if ingredient["ingredient"] == recipe_ingrediets["ingredient"] and \
-                ingredient["subcategory"] == recipe_ingrediets["subcategory"] and \
-                ingredient["amount"] == recipe_ingrediets["amount"]:
-                    return False
-        return True
+    # def check_ingredients(self, recipe: dict[str : str]) -> bool:
+    #     for entry in self.ingredient_file:
+    #         list_of_ingredients = entry["ingredients"]
+    #         recipe_ingredients = recipe["ingredients"]
+    #         if any(all(ingredient[key] == recipe_ingredient[key]) \
+    #         for key in ["ingredient", "subcategory", "amount"] for ingredient, recipe_ingredient \
+    #         in zip(list_of_ingredients, recipe_ingredients)):
+    #             return False
+    #     return True
 
     def parse_recipe_dictionary(self, recipe_dictionary: dict[str : str]) -> None:
-        list_of_functions = [self.check_ingredient_amount, self.check_ingredients, self.check_recipe_name]
+        list_of_functions = [self.check_ingredient_amount, self.check_recipe_name]
 
+        empty_list = []
+        empty_list.append(recipe_dictionary)
         if self.ingredient_file is None:
-            empty_list = []
-            empty_list.append(recipe_dictionary)
             with open(self.file_path, "w", encoding="utf-8") as file:
                 yaml.dump(empty_list, file, default_flow_style=False)
 
-        if all(func(recipe_dictionary) for func in list_of_functions): 
+        elif all(func(recipe_dictionary) for func in list_of_functions): 
             with open(self.file_path, "a", encoding="utf-8") as file:
-                yaml.dump(recipe_dictionary, file, default_flow_style=False)
+                yaml.dump(empty_list, file, default_flow_style=False)
 
         # else:
         #     if all(func(recipe_dictionary) for func in list_of_functions):
